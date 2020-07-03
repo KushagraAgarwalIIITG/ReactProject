36dd4eb51c6093400dfead5fe91b9477aff2dd31
Priya Pampati <priyapampati18@gmail.com>
2020-07-03 17:05:15 +0530

    Options, Statues added
    Signed-off-by: Priya Pampati <priyapampati18@gmail.com>

--- reactproject/src/components/TableStructure.js
+++ reactproject/src/components/TableStructure.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import { Button, Paper, TextField, TableRow, TableHead, TableContainer, TableCell, TableBody, Table } from '@material-ui/core';
+import { MenuItem, FormControl, Select } from '@material-ui/core'
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 import { Add, Create } from '@material-ui/icons';
+import { ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
+
 
 const theme = createMuiTheme({
     palette: {
         primary: {
-            main: '#daf7a6',
+            main: '#3f50b5',
+            light: '#daf7a6',
         },
         secondary: {
             main: '#c0fefb',
@@ -20,11 +24,14 @@ const theme = createMuiTheme({
 
 const StyledTableCell = withStyles((theme) => ({
     head: {
-        backgroundColor: theme.palette.primary.main,
+        backgroundColor: theme.palette.primary.light,
         color: theme.palette.common.black,
+        fontSize: 20,
     },
     body: {
-        fontSize: 9,
+        fontSize: 15,
+        padding: '10px',
+        alignContent: 'right',
     },
 }))(TableCell);
 
@@ -42,15 +49,22 @@ const StyledTableRow = withStyles((theme) => ({
 var rows =
     [
         { "id": 1, "date": "2000-08-21", "name": "Priya", "options": "yes", "status": 2 },
-        { "id": 2, "date": "1998-11-15", "name": "Kushagra", "options": "yes", "status": 1 },
+        { "id": 2, "date": "1998-11-15", "name": "Kushagra", "options": "no", "status": 1 },
     ]
 
 const useStyles = makeStyles({
     table: {
-        minWidth: 700,
+        minWidth: 600,
     },
     button: {
         margin: theme.spacing(1),
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
     },
 });
 
@@ -74,7 +88,7 @@ function TableStructure() {
                     <TableBody>
                         {rows.map((row) => (
                             <StyledTableRow key={row.name}>
-                                <StyledTableCell component="th" scope="row">
+                                <StyledTableCell align='justify'>
                                     <Button variant="contained"
                                         color="default"
                                         className={classes.button}
@@ -101,8 +115,29 @@ function TableStructure() {
                                     </form>
                                 </StyledTableCell>
                                 <StyledTableCell align="justify">{row.name}</StyledTableCell>
-                                <StyledTableCell align="justify">{row.options}</StyledTableCell>
-                                <StyledTableCell align="justify">{row.status}</StyledTableCell>
+                                <StyledTableCell align="justify">
+                                    <ToggleButtonGroup type="checkbox" value={row.options} className="mb-2">
+                                        <ToggleButton value={'yes'}>Yes</ToggleButton>
+                                        <ToggleButton value={'no'}>No</ToggleButton>
+                                    </ToggleButtonGroup>
+                                </StyledTableCell>
+                                <StyledTableCell align="justify">
+                                    <FormControl className={classes.formControl}>
+                                        <Select
+                                            defaultValue={row.status}
+                                            displayEmpty
+                                            className={classes.selectEmpty}
+                                            inputProps={{ 'aria-label': 'Without label' }}
+                                        >
+                                            <MenuItem value="" disabled>Select Status</MenuItem>
+                                            <MenuItem value={1}>1</MenuItem>
+                                            <MenuItem value={2}>2</MenuItem>
+                                            <MenuItem value={3}>3</MenuItem>
+                                            <MenuItem value={4}>4</MenuItem>
+                                            <MenuItem value={5}>5</MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                </StyledTableCell>
                             </StyledTableRow>
                         ))}
                     </TableBody>
